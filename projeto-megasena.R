55d81a380333926b211c4f20f88e744160cc5902
Bruno Marques <brunomaarques@hotmail.com.br>
2018-07-12 10:00:51 -0300

    base

---
+++ projeto-megasena.R
@@ -0,0 +1,3 @@
+#projeto megasena
+data<-read.csv2('C:\\Users\\Bruno\\Documents\\UFRPE\\Computação para Análise de Dados\\Códigos\\dados\\MEGASENA-03-07-2018.txt', sep='\t', dec=',', header=T, na.strings='', strip.white=T)
+View(data)
