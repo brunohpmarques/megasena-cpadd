6a7f580f2bd5a18d8f74a2adc55cd9d0f3c7a3f9
Bruno Marques <brunomaarques@hotmail.com.br>
2018-07-19 17:58:36 -0300

    Graficos; Mapa;

--- graficos.R
+++ graficos.R
@@ -1,10 +1,29 @@
 #graficos
+install.packages(c('maps','mapdata','rworldmap','maptools','mapproj','ggmap','rgdal'))
 library(dplyr)
 library(tidyr)
+library(maps) #mapas simples, eixos, escala, cidades 
+library(mapdata) #base de dados WorldHires e rios
+library(rworldmap) #outra base de dados de mapas do mundo
+library(maptools) #Ler ESRI shapefiles 
+library(mapproj) #Projeções e grids
+library(ggmap) #Gmaps, OSM + mapas baseados em ggplot2
+library(rgdal)
 
-data<-read.csv2('C:\\Users\\Bruno\\Documents\\UFRPE\\Computação para Análise de Dados\\Códigos\\projeto\\clean-data.csv', sep=';', dec='.', header=T, na.strings='', strip.white=T)
-data.dezenas<-read.csv2('C:\\Users\\Bruno\\Documents\\UFRPE\\Computação para Análise de Dados\\Códigos\\projeto\\clean-data-dezenas.csv', sep=';', dec='.', header=T, na.strings='', strip.white=T)
-data.estados<-read.csv2('C:\\Users\\Bruno\\Documents\\UFRPE\\Computação para Análise de Dados\\Códigos\\projeto\\clean-data-estados.csv', sep=';', dec='.', header=T, na.strings='', strip.white=T)
+projeto<-paste(getwd(), '/UFRPE/Computação para Análise de Dados/Códigos/projeto/', sep='')
+#projeto<-paste(getwd(), '...')
+
+#https://rstudio-pubs-static.s3.amazonaws.com/176768_ec7fb4801e3a4772886d61e65885fbdd.html
+par(mar=c(1,1,1,1))
+shape.estados<-readOGR(paste(projeto, "shapes/estados_2010.shp", sep=''))
+plot(shape.estados, main='Ganhadores por Estado', col='white')
+
+#https://plot.ly/r/choropleth-maps/
+#https://www.r-graph-gallery.com/175-choropleth-map-cartography-pkg/
+
+data<-read.csv2(paste(projeto,'clean-data.csv', sep=''), sep=';', dec='.', header=T, na.strings='', strip.white=T)
+data.dezenas<-read.csv2(paste(projeto,'clean-data-dezenas.csv', sep=''), sep=';', dec='.', header=T, na.strings='', strip.white=T)
+data.estados<-read.csv2(paste(projeto,'clean-data-estados.csv', sep=''), sep=';', dec='.', header=T, na.strings='', strip.white=T)
 
 View(data)
 
@@ -34,3 +53,15 @@ plot(x=ganhadoresAno$Ano_Sorteio, y=ganhadoresAno$Ganhadores_Sena, type='o', pch
 estados<-data.estados
 estados<-aggregate(Concurso ~ UF, FUN=length, estados)
 barplot(estados$Concurso, main='Ganhadores por estado', ylab='Ganhadores', xlab='UF', names.arg=estados$UF, col=rainbow(26))
+
+#pizza dos mais ganhadores - agrupar os menores 'outros'
+
+#ganhadores por quantidade de concursos
+ganhadoresConcurso<-data$Acumulado
+classes<-c('Ganhadores','Acumulado')
+x<-c(sum(!ganhadoresConcurso[TRUE]), sum(ganhadoresConcurso[TRUE]))
+ptc<-paste(round(x/sum(x)*100), '%', sep='')
+pie(x, ptc, main='Ganhadores x Acumulado', col=c('green','blue'))
+legend(x='topleft', legend=classes, fill=c('green','blue'), cex = 0.70)
+
+
