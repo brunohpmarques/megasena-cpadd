54fae99d8774d6f1e7bcf800ffcdb0fa01ddc841
Danielly Queiroz <danielly.queiroz@outlook.com>
2018-08-14 08:35:23 -0300

    Correlação linear entre as variaveis

--- regressao-linear.R
+++ regressao-linear.R
@@ -1,60 +1,15 @@
 projeto<-paste(getwd(), '/UFRPE/Computação para Análise de Dados/Códigos/projeto/', sep='')
+projeto<-paste(getwd(), '/', sep='')
 
 data<-read.csv2(paste(projeto,'clean-data.csv', sep=''), sep=';', dec='.', header=T, na.strings='', strip.white=T)
 data.dezenas<-read.csv2(paste(projeto,'clean-data-dezenas.csv', sep=''), sep=';', dec='.', header=T, na.strings='', strip.white=T)
 data.estados<-read.csv2(paste(projeto,'clean-data-estados.csv', sep=''), sep=';', dec='.', header=T, na.strings='', strip.white=T)
 
-#transformar os estados em números para poder fazer o teste de regressão linear
-Estados.Numericos<-data.estados
-levels(Estados.Numericos$UF)
-Estados.Numericos$UF<-as.factor(Estados.Numericos$UF)
-levels(Estados.Numericos$UF)<-1:26
-Estados.Numericos$UF<- as.numeric(Estados.Numericos$UF)
-View(Estados.Numericos)
-
-#----ESTIMATIVA DE ESTADOS GANHADORES----
-r.estadosGanhadores<-lm(UF ~ Concurso, data= Estados.Numericos)
-summary(r.estadosGanhadores)
-
-# A equação da linha de regressão estimada pode ser escrita 
-# da seguinte forma: UF = 17.37 -0.0004*Concurso
-
-# grafico da Regressão
-plot (UF ~ Concurso, pch=16 ,data =Estados.Numericos, main='Dispersão Concurso x UF', cex.axis=0.7)
-abline(r.estadosGanhadores,col="red")
-
-#avaliar Modelo
-
-#Estamos interessados em saber se X (Concurso) pode ou não ser utilizada
-#para predizer Y (UF). Se o valor de p associado ao coeficiente for igual ou
-#menor que o nível de significância adotado, então aceitamos que existe uma
-#relação estatísticas entre as variáveis. 
-
-summary(r.estadosGanhadores)
-
-#Como Concurso Pr(>|t|) = 0.36457 > 0.05 então eles tem relação estatísticas
-# R =  0.00125  o modelo de regressão não explicou grande parte da variabilidade no resultado.
-
-#Analise dos Pressuposto 
-# normalidade
-
-normalidade<-shapiro.test(residuals(r.estadosGanhadores))
-
-if(normalidade$p.value<=0.05){
-  print("Hipotese Nula Rejeitada:os resíduos não são provenientes de uma distribuição Normal.")
-}else{
-  print("Hipotese Nula Aceita: os resíduos são provenientes de uma distribuição Normal.")
-}
-
-# Variavel Homogenea
-plot(r.estadosGanhadores,1)
-
-previsao<-data.frame(Concurso=c(2001,2003,2010))
-predict(r.estadosGanhadores, previsao)
-
 #------- Estimativa de Premios ---------
+# Para confirmar a correlação entre as variáveis usaremos o Coeficiente de Correlação Linear de Pearson.
+cor.test(data$Concurso, data$Estimativa_Premio)
+# a correlação foi 0.45 então ela possui um grau fraco associado
 
-View(data)
 dataEstimativa<-data
 dataEstimativa$Estimativa_Premio<-round(dataEstimativa$Estimativa_Premio/1000000) #por milhoes de reais
 r.EstimativaPremio<- lm(Estimativa_Premio~Concurso, data=dataEstimativa)
@@ -77,7 +32,7 @@ abline(r.EstimativaPremio, col="red")
 summary(r.EstimativaPremio)
 
 #Como Concurso Pr(>|t|) = 2.22e-16 < 0.05 então eles tem relação estatísticas.
-# R =  0.2031  o modelo de regressão explicou grande parte da variabilidade no resultado.
+# R =  0.2031   o modelo de regressão não explicou grande parte da variabilidadeno resultado
 
 #Analise dos Pressuposto 
 # normalidade
@@ -101,6 +56,9 @@ predict(r.EstimativaPremio, previsao)
 #Em 2068 o valor está estimado em: 31693803.4729509 
 
 #------- Estimativa de Valor Acumulado ---------
+#Para confirmar a correlação entre as variáveis usaremos o Coeficiente de Correlação Linear de Pearson.
+cor.test(data$Concurso, data$Valor_Acumulado)
+# a correlação foi 0.28 então ela possui um grau fraco de associacao
 
 View(data)
 dataEstimativa<-data
@@ -125,7 +83,7 @@ abline(r.EstimativaValorAcumulado, col="red")
 summary(r.EstimativaValorAcumulado)
 
 #Como Concurso Pr(>|t|) = 2.22e-16 < 0.05 então eles tem relação estatísticas
-# R =  0.077  o modelo de regressão explicou grande parte da variabilidade no resultado.
+# R =  0.077   o modelo de regressão não explicou grande parte da variabilidade no resultado
 
 #Analise dos Pressuposto 
 # normalidade
@@ -149,6 +107,9 @@ predict(r.EstimativaValorAcumulado, previsao)
 #Em 2068 o valor acumulado está estimado em: 21011562.3985342 
 
 #------- Estimativa de Valor Acumulado MegaVirada ---------
+#Para confirmar a correlação entre as variáveis usaremos o Coeficiente de Correlação Linear de Pearson.
+cor.test(data$Concurso, data$Acumulado_Mega_da_Virada)
+# a correlação foi 0.70 então ela possui um grau moderado ou bom associado
 
 dataEstimativa<-data
 dataEstimativa$Acumulado_Mega_da_Virada<-round(dataEstimativa$Acumulado_Mega_da_Virada/1000000) #por milhoes de reais
@@ -171,7 +132,7 @@ abline(r.Acumulado_Mega_da_Virada, col="red")
 summary(r.Acumulado_Mega_da_Virada)
 
 #Como Concurso Pr(>|t|) = 2.22e-16 < 0.05 então eles tem relação estatísticas
-# R =  0.49  o modelo de regressão explicou grande parte da variabilidade no resultado.
+# R =  0.49   o modelo de regressão não explicou grande parte da variabilidade no resultado
 
 #Analise dos Pressuposto 
 # normalidade
@@ -195,96 +156,3 @@ predict(r.Acumulado_Mega_da_Virada, previsao)
 #Em 2020 o valor Acumulado da Mega da Virada está estimado em: 42728663.7842579
 #Em 2068 o valor Acumulado da Mega da Virada está estimado em: 44001540.7445984 
 
-#------- Estimativa do o numero de ganhadores pra algum concurso futuro ---------
-
-View(data)
-r.NumeroGanhadores<- lm(Ganhadores_Sena~Concurso, data=data)
-summary(r.NumeroGanhadores)
-# A equação da linha de regressão estimada pode ser escrita 
-# da seguinte forma: Concurso = -964.2 +  0.0054*Ganhadores_Quadra - 0.088*Ganhadores_Quina + 18.87*Ganhadores_Sena
-
-
-# grafico da Regressão
-plot (Ganhadores_Sena~Concurso, pch=16, data=data, main='Dispersão Concurso x Ganhadores', ylab='Ganhadores', cex.axis=0.7)
-abline(r.NumeroGanhadores, col="red")
-
-#avaliar Modelo
-
-#Estamos interessados em saber se X (Concurso) pode ou não ser utilizada
-#para predizer Y (Acumulado_Mega_da_Virada). Se o valor de p associado ao coeficiente for igual ou
-#menor que o nível de significância adotado, então aceitamos que existe uma
-#relação estatísticas entre as variáveis. 
-
-summary(r.NumeroGanhadores)
-
-#Como Concurso Pr(>|t|) = 0.0045 < 0.05 então eles tem relação estatísticas
-# R =  0.0065 o modelo de regressão explicou grande parte da variabilidade no resultado.
-
-#Analise dos Pressuposto 
-# normalidade
-
-normalidade<-shapiro.test(residuals(r.NumeroGanhadores))
-
-if(normalidade$p.value<=0.05){
-  print("Hipotese Nula Rejeitada:os resíduos não são provenientes de uma distribuição Normal.")
-}else{
-  print("Hipotese Nula Aceita: os resíduos são provenientes de uma distribuição Normal.")
-}
-
-# Variavel Homogenea
-plot(r.NumeroGanhadores,1)
-
-previsao<-data.frame(Concurso=c(1,529,2068))
-predict(r.NumeroGanhadores, previsao)
-
-#Previsão:
-#Em 2001 o valor Acumulado da Mega da Virada está estimado em: 42224816.6541231
-#Em 2020 o valor Acumulado da Mega da Virada está estimado em: 42728663.7842579
-#Em 2068 o valor Acumulado da Mega da Virada está estimado em: 44001540.7445984 
-
-#------- Estimar o quando o estado de SP ele vai ganhar-----
-Estados.Numericos
-r.GanhadoresSP<- lm(UF~Concurso, data=Estados.Numericos)
-r.GanhadoresSP<- lm(Concurso~UF, data=Estados.Numericos)
-summary(r.GanhadoresSP)
-# A equação da linha de regressão estimada pode ser escrita 
-# da seguinte forma: Concurso = -964.2 +  0.0054*Ganhadores_Quadra - 0.088*Ganhadores_Quina + 18.87*Ganhadores_Sena
-
-
-# grafico da Regressão
-plot (UF~Concurso, pch=16 ,data =Estados.Numericos)
-abline(r.GanhadoresSP,col="red")
-
-#avaliar Modelo
-
-#Estamos interessados em saber se X (Concurso) pode ou não ser utilizada
-#para predizer Y (Acumulado_Mega_da_Virada). Se o valor de p associado ao coeficiente for igual ou
-#menor que o nível de significância adotado, então aceitamos que existe uma
-#relação estatísticas entre as variáveis. 
-
-summary(r.GanhadoresSP)
-
-#Como Concurso Pr(>|t|) = 0.0045 < 0.05 então eles tem relação estatísticas
-# R =  0.0065 o modelo de regressão explicou grande parte da variabilidade no resultado.
-
-#Analise dos Pressuposto 
-# normalidade
-
-normalidade<-shapiro.test(residuals(r.GanhadoresSP))
-
-if(normalidade$p.value<=0.05){
-  print("Hipotese Nula Rejeitada:os resíduos não são provenientes de uma distribuição Normal.")
-}else{
-  print("Hipotese Nula Aceita: os resíduos são provenientes de uma distribuição Normal.")
-}
-
-# Variavel Homogenea
-plot(r.GanhadoresSP,1)
-
-previsao<-data.frame(UF=c(25))
-predict(r.GanhadoresSP, previsao, index=1:2)
-View(Estados.Numericos)
-#Previsão:
-#Em 2001 o valor Acumulado da Mega da Virada está estimado em: 42224816.6541231
-#Em 2020 o valor Acumulado da Mega da Virada está estimado em: 42728663.7842579
-#Em 2068 o valor Acumulado da Mega da Virada está estimado em: 44001540.7445984 
