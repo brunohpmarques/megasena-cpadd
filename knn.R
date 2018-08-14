eb1d26e09dcc0f5628a8ee5d7ed23c4fa1c40054
Bruno Marques <brunomaarques@hotmail.com.br>
2018-08-13 23:23:50 -0300

    prevenção de jogos usando knn

--- knn.R
+++ knn.R
@@ -8,15 +8,17 @@ projeto<-paste(getwd(), '/UFRPE/Computação para Análise de Dados/Códigos/pro
 #projeto<-paste(getwd(), '...')
 
 data<-read.csv2(paste(projeto,'clean-data.csv', sep=''), sep=';', dec='.', header=T, na.strings='', strip.white=T)
+data.dezenas<-read.csv2(paste(projeto,'clean-data-dezenas.csv', sep=''), sep=';', dec='.', header=T, na.strings='', strip.white=T)
 data.estados<-read.csv2(paste(projeto,'clean-data-estados.csv', sep=''), sep=';', dec='.', header=T, na.strings='', strip.white=T)
 
-data<-select(data, Concurso, Dia_Sorteio, Mes_Sorteio)
-data<-merge(data.estados, data, by='Concurso', all=TRUE)
+#knn para prever o estado ganhador a partir de uma data
+data.knn<-select(data, Concurso, Dia_Sorteio, Mes_Sorteio)
+data.knn<-merge(data.estados, data.knn, by='Concurso', all=TRUE)
 
-levels(data$UF)<-c(levels(data$UF), 'NENHUM')
-data$UF[is.na(data$UF)]<-'NENHUM'
+levels(data.knn$UF)<-c(levels(data.knn$UF), 'NENHUM')
+data.knn$UF[is.na(data.knn$UF)]<-'NENHUM'
 
-train<-select(data, UF, Dia_Sorteio, Mes_Sorteio)
+train<-select(data.knn, -Concurso)
 
 #mes 1 pra PE e 3 pra SP e PR
 dia<-c(seq(1,31,1))
@@ -24,3 +26,20 @@ mes<-c(rep(1,31))
 test<-data.frame(UF=NA, Dia_Sorteio=dia, Mes_Sorteio=mes)
 print(dia)
 kNN(UF ~ ., train, test, norm=TRUE, k=3)
+
+
+
+#knn pra prever se um jogo ganharia ou nao
+data.dezenas<-spread(data.dezenas, key='Ordem_Sorteio', value='Dezena')
+data.knn<-select(data, Concurso, Acumulado)
+data.knn<-merge(data.dezenas, data.knn, by='Concurso')
+
+train<-select(data.knn, -Concurso)
+acumula<-TRUE
+#while (acumula) {
+dezenas<-sort(sample(1:60, 6)) # gera jogo aleatorio
+test<-data.frame('1'=dezenas[1], '2'=dezenas[2], '3'=dezenas[3], '4'=dezenas[4], '5'=dezenas[5], '6'=dezenas[6], Acumulado=NA)
+acumula<-as.logical(kNN(Acumulado ~ ., train, test, norm=TRUE, k=3))
+#}
+print(acumula)
+print(dezenas)
